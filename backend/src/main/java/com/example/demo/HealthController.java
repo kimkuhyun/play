f52ce7d007779c88820824893a1afdff7ca338b5
kimkuhyun <rngus225@gmail.com>
2025-12-21 20:50:05 +0900

    first commit

---
+++ backend/src/main/java/com/example/demo/HealthController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController{
+
+    @GetMapping("/")
+    public String root(){
+        return "OK";
+    }
+
+    @GetMapping("/health")
+    public String health(){
+        return "OK";
+    }
+
+}
